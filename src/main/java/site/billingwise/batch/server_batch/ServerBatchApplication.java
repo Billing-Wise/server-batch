b16d93412d55648530c5b68ba4612a5b2934f9a2
변현진 <78016592+shinbm44@users.noreply.github.com>
2024-07-08 16:49:29 +0900

    initial commit

---
+++ src/main/java/site/billingwise/batch/server_batch/ServerBatchApplication.java
@@ -0,0 +1,13 @@
+package site.billingwise.batch.server_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/billingwise/batch/server_batch/ServerBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package site.billingwise.batch.server_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
